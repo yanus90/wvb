feccd4a281b0bd47649fcff763984776dedd96bc
Yanick Nagtegaal <ynagtegaal@avancecommunicatie.nl>
2019-10-09 21:09:17 +0200

    Template

--- web/app/themes/wvb-theme/app/Controllers/SingleElftal.php
+++ web/app/themes/wvb-theme/app/Controllers/SingleElftal.php
@@ -6,9 +6,67 @@ use Sober\Controller\Controller;
 
 class SingleElftal extends Controller
 {
+//    private $sportlink;
+//
+//    public function __construct()
+//    {
+//        $this->sportlink = get_field('sportlink_gegevens', get_queried_object_id());
+//    }
+
     function ranking()
     {
         $ranking = [];
+
+        $array = get_field('sportlink_gegevens', get_queried_object_id());
+        $teamcode = $array['sportlink_api_id'] ?? null;
+        $poulecode = $array['sportlink_poulecode'] ?? null;
+
+        try {
+            $json = file_get_contents('https://data.sportlink.com/poulestand?clientId=X5wbJgu0J7&poulecode=' . $poulecode);
+        } catch (\Exception $e) {
+            $json = null;
+        }
+
+        if ($json !== false) {
+            $file = file_put_contents(get_template_directory().'/storage/team-ranking-'.$teamcode.'.json', $json);
+        } else {
+            $file = file_get_contents(get_template_directory().'/storage/team-ranking-'.$teamcode.'.json');
+            $json = json_encode($file, true);
+        }
+
+        if ($json) {
+            $ranking = json_decode($json, true);
+        }
+
+        return $ranking;
+    }
+
+    function period()
+    {
+        $period = [];
+
+        $array = get_field('sportlink_gegevens', get_queried_object_id());
+        $teamcode = $array['sportlink_api_id'] ?? null;
+        $poulecode = $array['sportlink_poulecode'] ?? null;
+
+        try {
+            $json = file_get_contents('https://data.sportlink.com/periodestand?clientId=X5wbJgu0J7&poulecode=' . $poulecode);
+        } catch (\Exception $e) {
+            $json = null;
+        }
+
+        if ($json !== false) {
+            $file = file_put_contents(get_template_directory().'/storage/team-period-'.$teamcode.'.json', $json);
+        } else {
+            $file = file_get_contents(get_template_directory().'/storage/team-period-'.$teamcode.'.json');
+            $json = json_encode($file, true);
+        }
+
+        if ($json) {
+            $period = json_decode($json, true);
+        }
+
+        return $period;
     }
 
     function program()
@@ -39,4 +97,61 @@ class SingleElftal extends Controller
 
         return $results;
     }
+
+    function results()
+    {
+        $results = [];
+
+        $array = get_field('sportlink_gegevens', get_queried_object_id());
+        $teamcode = $array['sportlink_api_id'] ?? null;
+        $poulecode = $array['sportlink_poulecode'] ?? null;
+
+        try {
+            $json = file_get_contents('https://data.sportlink.com/pouleuitslagen?clientId=X5wbJgu0J7&poulecode=' . $poulecode . '&weekoffset=-2');
+        } catch (\Exception $e) {
+            $json = null;
+        }
+
+        if ($json !== false) {
+            $file = file_put_contents(get_template_directory().'/storage/team-results-'.$teamcode.'.json', $json);
+        } else {
+            $file = file_get_contents(get_template_directory().'/storage/team-results-'.$teamcode.'.json');
+            $json = json_encode($file, true);
+        }
+
+        if ($json) {
+            $results = json_decode($json, true);
+        }
+
+        return $results;
+    }
+
+    function resultsCup()
+    {
+        $results_cup = [];
+
+        $array = get_field('sportlink_gegevens', get_queried_object_id());
+        $teamcode = $array['sportlink_api_id'] ?? null;
+        $poulecode_cup = $array['sportlink_bekercode'] ?? null;
+
+        try {
+            $json = file_get_contents('https://data.sportlink.com/pouleuitslagen?clientId=X5wbJgu0J7&teamcode=' . $teamcode . '&poulecode=' . $poulecode_cup);
+        } catch (\Exception $e) {
+            $json = null;
+        }
+
+        if ($json !== false) {
+            $file = file_put_contents(get_template_directory().'/storage/team-results-cup-'.$teamcode.'.json', $json);
+        } else {
+            $file = file_get_contents(get_template_directory().'/storage/team-results-cup-'.$teamcode.'.json');
+            $json = json_encode($file, true);
+        }
+
+        if ($json) {
+            $results_cup = json_decode($json, true);
+        }
+
+        return $results_cup;
+    }
+
 }
--- web/app/themes/wvb-theme/resources/views/partials/team/team-data.blade.php
+++ web/app/themes/wvb-theme/resources/views/partials/team/team-data.blade.php
@@ -1,16 +1,21 @@
 <div class="team-tabs">
-    <ul class="nav nav-pills" id="team-data" role="tablist">
+    <ul class="nav nav-pills mb-3" id="team-data" role="tablist">
         <li class="nav-item">
-            <a class="nav-link active" id="ranking" data-toggle="tab" href="#ranking" role="tab" aria-controls="ranking" aria-selected="false"><i class="fal fa-list-ol mr-2"></i>Stand</a>
+            <a class="nav-link active" data-toggle="tab" href="#ranking"><i class="fal fa-list-ol mr-2"></i>Stand</a>
+        </li>
+        @if($post->post_name == 'warnsveldse-boys-1')
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="tab" href="#period"><i class="fal fa-list-ol mr-2"></i>Periode</a>
+        </li>
+        @endif
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="tab" href="#program"><i class="fal fa-calendar-alt mr-2"></i>Programma</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" id="program" data-toggle="tab" href="#program" role="tab" aria-controls="program" aria-selected="false"><i class="fal fa-calendar-alt mr-2"></i>Programma</a>
+            <a class="nav-link" data-toggle="tab" href="#results"><i class="fas fa-futbol mr-2"></i>Uitslagen</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" id="results" data-toggle="tab" href="#results" role="tab" aria-controls="results" aria-selected="false"><i class="fas fa-futbol mr-2"></i>Uitslagen</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" id="cup" data-toggle="tab" href="#cup" role="tab" aria-controls="cup" aria-selected="false"><i class="fal fa-trophy-alt mr-2"></i>Beker</a>
+            <a class="nav-link" data-toggle="tab" href="#cup"><i class="fal fa-trophy-alt mr-2"></i>Beker</a>
         </li>
     </ul>
     <div class="tab-content" id="team-data-content">
@@ -32,11 +37,62 @@
                         </tr>
                     </thead>
                     <tbody>
-
+                        @foreach($ranking as $rank)
+                            <tr class="{{ ($rank['eigenteam'] == "true" ? 'bg-primary color-white' : '') }}">
+                                <td>{!! $rank['positie'] !!}</td>
+                                <td>{!! $rank['teamnaam'] !!}</td>
+                                <td>{!! $rank['gespeeldewedstrijden'] !!}</td>
+                                <td>{!! $rank['gewonnen'] !!}</td>
+                                <td>{!! $rank['gelijk'] !!}</td>
+                                <td>{!! $rank['verloren'] !!}</td>
+                                <td>{!! $rank['doelpuntenvoor'] !!}</td>
+                                <td>{!! $rank['doelpuntentegen'] !!}</td>
+                                <td>{!! $rank['doelsaldo'] !!}</td>
+                                <td>{!! $rank['punten'] !!}</td>
+                            </tr>
+                        @endforeach
                     </tbody>
                 </table>
             </div>
         </div>
+        @if($post->post_name == 'warnsveldse-boys-1')
+            <div class="tab-pane fade" id="period" role="tabpanel" aria-labelledby="period-tab">
+                <div class="table-responsive">
+                    <table class="table table-striped">
+                        <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Team</th>
+                            <th>GW</th>
+                            <th>W</th>
+                            <th>G</th>
+                            <th>V</th>
+                            <th>DV</th>
+                            <th>DT</th>
+                            <th>S</th>
+                            <th>P</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($period as $period_item)
+                            <tr class="{{ ($period_item['eigenteam'] == "true" ? 'bg-primary color-white' : '') }}">
+                                <td>{!! $period_item['positie'] !!}</td>
+                                <td>{!! $period_item['teamnaam'] !!}</td>
+                                <td>{!! $period_item['aantalwedstrijden'] !!}</td>
+                                <td>{!! $period_item['gewonnen'] !!}</td>
+                                <td>{!! $period_item['gelijkspel'] !!}</td>
+                                <td>{!! $period_item['verloren'] !!}</td>
+                                <td>{!! $period_item['doelpuntenvoor'] !!}</td>
+                                <td>{!! $period_item['tegendoelpunten'] !!}</td>
+                                <td>{!! $period_item['doelsaldo'] !!}</td>
+                                <td>{!! $period_item['totaalpunten'] !!}</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        @endif
         <div class="tab-pane fade" id="program" role="tabpanel" aria-labelledby="program-tab">
             <div class="table-responsive">
                 <table class="table table-striped">
@@ -60,6 +116,33 @@
                     </tbody>
                 </table>
             </div>
+        </div>
+        <div class="tab-pane fade" id="results" role="tabpanel" aria-labelledby="results-tab">
+            <div class="table-responsive">
+                <table class="table table-striped">
+                    <thead>
+                    <tr>
+                        <th>Datum</th>
+                        <th>Tijd</th>
+                        <th>Wedstrijd</th>
+                        <th>Uitslag</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($results as $result)
+                        <tr>
+                            <td>{!! $result['datumopgemaakt'] !!}</td>
+                            <td>{!! $result['aanvangstijd'] !!}</td>
+                            <td>{!! $result['wedstrijd'] !!}</td>
+                            <td>{!! $result['uitslag'] !!}</td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="tab-pane fade" id="cup" role="tabpanel" aria-labelledby="cup-tab">
+            Beker
         </div>
     </div>
 </div>
