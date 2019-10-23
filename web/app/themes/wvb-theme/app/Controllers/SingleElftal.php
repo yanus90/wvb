d2ab45c56c8e03ccb016259a108449548067815e
yanus90 <yan.uss@hotmail.com>
2019-10-23 09:54:48 +0200

    Aanpassing class functies

--- web/app/themes/wvb-theme/app/Controllers/SingleElftal.php
+++ web/app/themes/wvb-theme/app/Controllers/SingleElftal.php
@@ -6,42 +6,26 @@ use Sober\Controller\Controller;
 
 class SingleElftal extends Controller
 {
-//    private $sportlink;
-//
-//    public function __construct()
-//    {
-//        $this->sportlink = get_field('sportlink_gegevens', get_queried_object_id());
-//    }
+    private $sportlink_settings;
+    private $teamcode;
+    private $poulecode;
 
-    function ranking()
+    public function __construct()
     {
-        $ranking = [];
-
-        $array = get_field('sportlink_gegevens', get_queried_object_id());
-        $teamcode = $array['sportlink_api_id'] ?? null;
-        $poulecode = $array['sportlink_poulecode'] ?? null;
-
-        try {
-            $json = file_get_contents('https://data.sportlink.com/poulestand?clientId=X5wbJgu0J7&poulecode=' . $poulecode);
-        } catch (\Exception $e) {
-            $json = null;
-        }
-
-        if ($json !== false) {
-            $file = file_put_contents(get_template_directory().'/storage/team-ranking-'.$teamcode.'.json', $json);
-        } else {
-            $file = file_get_contents(get_template_directory().'/storage/team-ranking-'.$teamcode.'.json');
-            $json = json_encode($file, true);
-        }
-
-        if ($json) {
-            $ranking = json_decode($json, true);
-        }
-
-        return $ranking;
+        $this->sportlink_settings = get_field('sportlink_gegevens', get_queried_object_id());
+        
+        $this->teamcode = $this->sportlink_settings['sportlink_api_id'] ?? null;
+        $this->poulecode = $this->sportlink_settings['sportlink_poulecode'] ?? null;
     }
 
-    function period()
+    public function ranking()
+    {
+        $file = get_template_directory().'/storage/team-ranking-'.$this->teamcode.'.json';
+        
+        return $this->getFileFromExternalLink($file);
+    }
+
+    public function period()
     {
         $period = [];
 
@@ -69,7 +53,7 @@ class SingleElftal extends Controller
         return $period;
     }
 
-    function program()
+    public function program()
     {
         $results = [];
 
@@ -98,7 +82,7 @@ class SingleElftal extends Controller
         return $results;
     }
 
-    function results()
+    public function results()
     {
         $results = [];
 
@@ -126,7 +110,7 @@ class SingleElftal extends Controller
         return $results;
     }
 
-    function resultsCup()
+    public function resultsCup()
     {
         $results_cup = [];
 
@@ -152,6 +136,30 @@ class SingleElftal extends Controller
         }
 
         return $results_cup;
+    }
+    
+    private function getFileFromExternalLink($file_path) 
+    {
+        $results = [];
+
+        try {
+            $json = file_get_contents('https://data.sportlink.com/periodestand?clientId=X5wbJgu0J7&poulecode=' . $this->poulecode);
+        } catch (\Exception $e) {
+            $json = null;
+        }
+
+        if ($json !== false) {
+            $file = file_put_contents($file_path, $json);
+        } else {
+            $file = file_get_contents($file_path);
+            $json = json_encode($file, true);
+        }
+
+        if ($json) {
+            $results = json_decode($json, true);
+        }
+
+        return $results;
     }
 
 }
