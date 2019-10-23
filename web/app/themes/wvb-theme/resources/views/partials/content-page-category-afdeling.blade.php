80f4677a47bb32b6ae3e2b5ebbca7f139252c2a5
Yanick Nagtegaal <ynagtegaal@avancecommunicatie.nl>
2019-10-23 21:37:47 +0200

    Template

--- web/app/themes/wvb-theme/resources/views/partials/content-page-category-afdeling.blade.php
+++ web/app/themes/wvb-theme/resources/views/partials/content-page-category-afdeling.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             @foreach ($elftallen as $key => $elftal)
-                <div class="col-12 col-md-6 col-lg-4 col-xxl-3 mb-4">
+                <div class="col-12 col-md-6 col-lg-4 mb-4">
                     @php $image = get_field('afbeelding', $elftal->ID) @endphp
                     <a href="{{ get_permalink($elftal) }}" class="card" title="{!! $elftal->post_title !!}">
                         <img src="{!! wp_get_attachment_image_url($image['id'], 'team_thumb') !!}" class="card-img" alt="{{ $elftal->post_title }}">
--- web/app/themes/wvb-theme/resources/views/template-elftallen.blade.php
+++ web/app/themes/wvb-theme/resources/views/template-elftallen.blade.php
@@ -14,7 +14,7 @@
         <div class="container">
             <div class="row">
                 @foreach($categories as $category)
-                    <div class="col-12 col-md-6 col-lg-4 mb-4">
+                    <div class="col-12 col-md-6 col-lg-4 col-xxl-3 mb-4">
                         @php $image = get_field('afbeelding', 'term_'.$category->term_id) @endphp
                         <a href="/elftallen/{!! $category->slug !!}" class="card" title="{{ $category->name }}">
                             <img src="{!! wp_get_attachment_image_url($image['id'], 'team_thumb') !!}" class="card-img" alt="{{ $category->name }}">
