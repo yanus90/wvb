4bbb3ae271250906173bd2eca8e8e89854c02c42
Yanick Nagtegaal <ynagtegaal@avancecommunicatie.nl>
2019-10-26 14:20:51 +0200

    Template

---
+++ web/app/themes/wvb-theme/resources/views/partials/featured-news.blade.php
@@ -0,0 +1,14 @@
+<ul class="list-unstyled mb-3 mb-md-4">
+    @foreach($latest_news_articles as $article)
+        <li class="mb-3">
+            <a href="{!! get_permalink($article) !!}" title="{!! $article->post_title !!}" class="media text-decoration-none">
+                <img src="{!! ($article->overzichtafbeelding ? wp_get_attachment_image_url($article->overzichtafbeelding, 'thumbnail') : '//placehold.it/150x150') !!}" class="mr-3 img-fluid rounded" alt="{!! $article->post_title !!}" width="70">
+                <div class="media-body font-size-p16 line-height-regular">
+                    <h6 class="date small mb-1 color-black">{{ get_the_date('', $article) }}</h6>
+                    <h5 class="">{!! $article->post_title !!}</h5>
+                </div>
+            </a>
+        </li>
+    @endforeach
+</ul>
+<p class="mb-0"><a href="/nieuws" class="btn btn-sm btn-outline-primary" title="Meer nieuws">Meer nieuws<i class="fal fa-angle-right ml-3"></i></a></p>
--- web/app/themes/wvb-theme/resources/views/partials/program-and-news.blade.php
+++ web/app/themes/wvb-theme/resources/views/partials/program-and-news.blade.php
@@ -1,22 +1,27 @@
 <div class="section section-featured-news bg-dark-light">
     <div class="container">
         <div class="row">
-            <div class="col-lg-6">
+            <div class="col-lg-8 col-xl-7 col-xxl-8">
                 <ul class="nav nav-pills" id="programResults" role="tablist">
                     @if($results)
                         <li class="nav-item">
-                            <a class="nav-link active font-size-lg-p20" id="results-tab" data-toggle="tab" href="#results" role="tab" aria-controls="results" aria-selected="false"><i class="fal fa-futbol mr-2"></i>Uitslagen</a>
+                            <a class="nav-link active font-size-p14 font-size-md-p16 font-size-lg-p20" id="results-tab" data-toggle="tab" href="#results" role="tab" aria-controls="results" aria-selected="false"><i class="fal fa-futbol mr-2"></i>Uitslagen</a>
                         </li>
                     @endif
                     @if($program)
                         <li class="nav-item">
-                            <a class="nav-link font-size-lg-p20" id="program-tab" data-toggle="tab" href="#program" role="tab" aria-controls="program" aria-selected="true"><i class="fal fa-calendar-alt mr-2"></i>Programma</a>
+                            <a class="nav-link font-size-p14 font-size-md-p16 font-size-lg-p20" id="program-tab" data-toggle="tab" href="#program" role="tab" aria-controls="program" aria-selected="true"><i class="fal fa-calendar-alt mr-2"></i>Programma</a>
+                        </li>
+                    @endif
+                    @if($latest_news_articles)
+                        <li class="nav-item d-lg-none">
+                            <a class="nav-link font-size-p14 font-size-md-p16 font-size-lg-p20" id="news-tab" data-toggle="tab" href="#news" role="tab" aria-controls="news" aria-selected="true"><i class="fal fa-newspaper mr-2"></i>Nieuws</a>
                         </li>
                     @endif
                 </ul>
                 <div class="tab-content" id="programResultsContent">
                     @if($results)
-                        <div class="tab-pane fade show active" id="results" role="tabpanel" aria-labelledby="results-tab">
+                        <div class="tab-pane fade show active results" id="results" role="tabpanel" aria-labelledby="results-tab">
                             <div class="table-responsive">
                                 <table class="table">
                                     <thead>
@@ -42,7 +47,7 @@
                         </div>
                     @endif
                     @if($program)
-                        <div class="tab-pane fade" id="program" role="tabpanel" aria-labelledby="program-tab">
+                        <div class="tab-pane fade program" id="program" role="tabpanel" aria-labelledby="program-tab">
                             <div class="table-responsive">
                                 <table class="table">
                                     <thead>
@@ -67,58 +72,27 @@
                             </div>
                         </div>
                     @endif
+                    @if($latest_news_articles)
+                        <div class="tab-pane fade" id="news" role="tabpanel" aria-labelledby="news-tab">
+                            @include('partials.featured-news')
+                        </div>
+                    @endif
                 </div>
             </div>
             @if ($latest_news_articles)
-                <div class="col-lg-6">
-                    <ul class="nav nav-pills mb-4">
+                <div class="d-none d-lg-inline-block col-lg-4 col-xl-5 col-xxl-4">
+                    <ul class="nav nav-pills mb-2">
                         <li class="nav-item">
                             <span class="nav-link active font-size-lg-p20"><i class="fal fa-newspaper mr-2"></i>Nieuws</span>
                         </li>
                     </ul>
                     <div class="row">
                         <div class="col-12">
-                            <ul class="list-unstyled mb-3 mb-md-4">
-                                @foreach($latest_news_articles as $article)
-                                    <li class="mb-3">
-                                        <a href="{!! get_permalink($article) !!}" title="{!! $article->post_title !!}" class="media text-decoration-none">
-                                            <img src="{!! ($article->overzichtafbeelding ? wp_get_attachment_image_url($article->overzichtafbeelding, 'thumbnail') : '//placehold.it/150x150') !!}" class="mr-3 img-fluid rounded" alt="{!! $article->post_title !!}" width="70">
-                                            <div class="media-body font-size-p16 line-height-regular">
-                                                <h6 class="date small mb-1 color-black">{{ get_the_date('', $article) }}</h6>
-                                                <h5 class="">{!! $article->post_title !!}</h5>
-                                            </div>
-                                        </a>
-                                    </li>
-                                @endforeach
-                            </ul>
-                            <p class="mb-0"><a href="/nieuws" class="btn btn-sm btn-outline-primary" title="Meer nieuws">Meer nieuws<i class="fal fa-angle-right ml-3"></i></a></p>
+                            @include('partials.featured-news')
                         </div>
                     </div>
                 </div>
             @endif
-            {{--@if ($events)--}}
-                {{--<div class="col-lg-4">--}}
-                    {{--<h3 class="color-black mb-3 mb-md-4 h4">Agenda</h3>--}}
-                    {{--<div class="row">--}}
-                        {{--<div class="col-12">--}}
-                            {{--<ul class="list-unstyled mb-3 mb-md-4">--}}
-                                {{--@foreach($events as $event)--}}
-                                    {{--<li class="mb-3">--}}
-                                        {{--<a href="{!! get_permalink($event) !!}" title="{!! $event->post_title !!}" class="media text-decoration-none">--}}
-                                            {{--<img src="{!! ($event->overzichtafbeelding ? wp_get_attachment_image_url($event->overzichtafbeelding, 'thumbnail') : '//placehold.it/150x150') !!}" class="mr-3 img-fluid rounded" alt="{!! $event->post_title !!}" width="60">--}}
-                                            {{--<div class="media-body font-size-p16 line-height-regular">--}}
-                                                {{--<h6 class="date small mb-1 color-black">{{ $event->datum }}</h6>--}}
-                                                {{--<h5 class="h6">{!! $event->post_title !!}</h5>--}}
-                                            {{--</div>--}}
-                                        {{--</a>--}}
-                                    {{--</li>--}}
-                                {{--@endforeach--}}
-                            {{--</ul>--}}
-                            {{--<p class="mb-0"><a href="/de-club/agenda/" class="btn btn-sm btn-outline-primary" title="Meer evenementen">Meer<i class="fal fa-angle-right ml-3"></i></a></p>--}}
-                        {{--</div>--}}
-                    {{--</div>--}}
-                {{--</div>--}}
-            {{--@endif--}}
         </div>
     </div>
 </div>
