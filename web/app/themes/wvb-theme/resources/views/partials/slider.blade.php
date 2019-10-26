11cd52ed29b2ae1b615e8d68d90fb1df7e81cae3
Yanick Nagtegaal <ynagtegaal@avancecommunicatie.nl>
2019-10-26 15:33:06 +0200

    Template

--- web/app/themes/wvb-theme/resources/views/partials/slider.blade.php
+++ web/app/themes/wvb-theme/resources/views/partials/slider.blade.php
@@ -1,8 +1,8 @@
-<div class="section section-slider pt-5">
+<div class="section section-slider pt-5 pb-lg-3 pb-xl-5">
     <div class="container">
         <div class="row">
             @if (get_field('homepagina_slider'))
-                <div class="col-xl-8 col-xxl-9 mb-4 mb-xl-0">
+                <div class="col-lg-8 col-xl-9 col-xxl-9 mb-4 mb-xl-0">
                     <div id="carouselHome" class="carousel slide" data-ride="carousel">
                         <ol class="carousel-indicators">
                             @foreach ($slider as $key => $slide)
@@ -13,8 +13,11 @@
                             @foreach ($slider as $key => $slide)
                                 <a href="{{ $slide['link'] }}" class="carousel-item @if ($key < 1) active @endif" title="{!! $slide['titel'] !!}" target="{{ ($slide['nieuw_tabblad'] == '_blank' ? '_blank' : 'self') }}">
                                     <img src="{!! wp_get_attachment_image_url($slide['afbeelding']['ID'], 'slider_image_crop') !!}" class="d-block w-100" alt="{!! $slide['afbeelding']['alt'] !!}">
-                                    <div class="carousel-caption d-none d-md-block">
-                                        <h3>{!! $slide['titel'] !!}</h3>
+                                    <div class="carousel-caption">
+                                        <h3 class="font-size-p20 font-size-sm-p22 font-size-md-p24 font-size-lg-p26 {{ ($slide['korte_intro'] ? 'mb-1' : '') }}">{!! $slide['titel'] !!}</h3>
+                                        @if($slide['korte_intro'])
+                                            <p class="mb-0 font-size-p14 font-size-md-p16">{!! $slide['korte_intro'] !!}</p>
+                                        @endif
                                     </div>
                                 </a>
                             @endforeach
@@ -22,18 +25,17 @@
                     </div>
                 </div>
             @endif
-            <div class="col-xl-4 col-xxl-3">
+            <div class="col-lg-4 col-xl-3 col-xxl-3">
                 <div class="row">
                     <div class="col-sm-12 col-md-12 col-xl-12">
                         <a href="#" class="card card-sponsor-featured mb-4">
                             <div class="card-body">
                                 <div class="row">
                                     <div class="col-12">
-                                        <img src="//placehold.it/280x175" class="img-fluid mb-3">
+                                        <img src="//placehold.it/280x175" class="img-fluid w-100 mb-2">
                                     </div>
                                     <div class="col-12 pt-2">
-                                        <h5 class="card-title mb-2">Sponsor van de maand</h5>
-                                        <p class="mb-0">Lees verder ></p>
+                                        <h5 class="card-title mb-0 font-size-xl-p18 font-size-xxl-p20">Sponsor van de maand</h5>
                                     </div>
                                 </div>
                                 {{--<h5 class="card-title color-white">Lid worden bij Warnsveldse Boys!</h5>--}}
