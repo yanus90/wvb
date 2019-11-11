48e49b685fa769e0d1c90a97c53e6127054e7bcc
Yanick Nagtegaal <ynagtegaal@avancecommunicatie.nl>
2019-11-11 21:54:30 +0100

    Template met contactpersonen

--- web/app/themes/wvb-theme/resources/views/partials/content-page-with-persons.blade.php
+++ web/app/themes/wvb-theme/resources/views/partials/content-page-with-persons.blade.php
@@ -14,11 +14,21 @@
                             <div class="row">
                                 @foreach($persons as $person)
                                     <div class="col-12 col-md-6 col-lg-12">
-                                        @php $image = get_field('profielfoto', $person); @endphp
+                                        @php
+                                            $image = get_field('profielfoto', $person);
+                                            $functions = wp_get_post_terms($person->ID, 'functie');
+                                        @endphp
                                         <div class="media mb-4">
                                             <img src="{!! ($image ? wp_get_attachment_image_url($image['id'], 'thumbnail') : '//placehold.it/100x100?text='.$person->post_title) !!}" class="mr-3 rounded-circle" width="100" alt="{!! $person->post_title !!}">
-                                            <div class="media-body pt-3">
-                                                <h5 class="mt-0 text-primary">{!! $person->post_title !!}</h5>
+                                            <div class="media-body">
+                                                <h5 class="mt-0 mb-0 text-primary">{!! $person->post_title !!}</h5>
+                                                @if($functions)
+                                                    <ul class="list-inline font-size-p14 mb-2">
+                                                        @foreach($functions as $function)
+                                                            <li class="list-inline-item">{!! $function->name !!}@if(! $loop->last), @endif</li>
+                                                        @endforeach
+                                                    </ul>
+                                                @endif
                                                 @if($person->telefoonnummer || $person->e_mailadres)
                                                     <ul class="fa-ul ml-4 font-size-p16">
                                                         @if($person->telefoonnummer)
