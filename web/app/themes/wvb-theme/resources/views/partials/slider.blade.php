70d838631b240e69c0a8d45302ff03dace43ea92
Yanick Nagtegaal <ynagtegaal@avancecommunicatie.nl>
2019-11-19 20:44:13 +0100

    Homeslider uitbreiden met verschillende soorten link types

--- web/app/themes/wvb-theme/resources/views/partials/slider.blade.php
+++ web/app/themes/wvb-theme/resources/views/partials/slider.blade.php
@@ -11,7 +11,19 @@
                         </ol>
                         <div class="carousel-inner">
                             @foreach ($slider as $key => $slide)
-                                <a href="{{ $slide['link'] }}" class="carousel-item @if ($key < 1) active @endif" title="{!! $slide['titel'] !!}" target="{{ ($slide['nieuw_tabblad'] == '_blank' ? '_blank' : '_self') }}">
+                                @php
+                                    $link = '';
+                                    if($slide['soort_link'] == 'post_item') {
+                                        $link = get_permalink($slide['nieuwsbericht']);
+                                    } elseif($slide['soort_link'] == 'page') {
+                                        $link = $slide['pagina'];
+                                    } elseif($slide['soort_link'] == 'external_link') {
+                                        $link = $slide['externe_url'];
+                                    } else {
+                                        $link = '#';
+                                    }
+                                @endphp
+                                <a href="{!! $link !!}" class="carousel-item @if ($key < 1) active @endif" title="{!! $slide['titel'] !!}" target="{{ ($slide['nieuw_tabblad'] == '_blank' ? '_blank' : '_self') }}">
                                     <img src="{!! wp_get_attachment_image_url($slide['afbeelding']['ID'], 'slider_image_crop') !!}" class="d-block w-100" alt="{!! $slide['afbeelding']['alt'] !!}">
                                     <div class="carousel-caption">
                                         <h3 class="font-size-p20 font-size-sm-p22 font-size-md-p24 font-size-lg-p26 {{ ($slide['korte_intro'] ? 'mb-1' : '') }}">{!! $slide['titel'] !!}</h3>
