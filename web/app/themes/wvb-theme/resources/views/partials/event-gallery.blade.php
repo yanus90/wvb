47c432ecdcc55384661b675c11103ea70ecac4ab
Yanick Nagtegaal <ynagtegaal@avancecommunicatie.nl>
2019-11-19 22:34:49 +0100

    Gallerij event

--- web/app/themes/wvb-theme/resources/views/partials/event-gallery.blade.php
+++ web/app/themes/wvb-theme/resources/views/partials/event-gallery.blade.php
@@ -1,6 +1,6 @@
 @php $gallery = get_field('galerij') @endphp
 @if ($gallery)
-    <div class="row">
+    <div class="row mt-4 mt-lg-5">
         @foreach($gallery as $item)
             <div class="col-4 col-sm-3 col-xl-2 mb-4">
                 <a href="{!! wp_get_attachment_image_url($item['ID'], 'large') !!}" data-fancybox="gallery" title="{{ get_post_meta($item['ID'], '_wp_attachment_image_alt', true) }}">
