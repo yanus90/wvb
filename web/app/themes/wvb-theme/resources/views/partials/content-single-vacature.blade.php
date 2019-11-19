89a816af2c92e2de51a591a86080795a89fd96d6
Yanick Nagtegaal <ynagtegaal@avancecommunicatie.nl>
2019-11-19 22:02:53 +0100

    Vacature fix

--- web/app/themes/wvb-theme/resources/views/partials/content-single-vacature.blade.php
+++ web/app/themes/wvb-theme/resources/views/partials/content-single-vacature.blade.php
@@ -5,7 +5,7 @@
                 @if($post->post_content)
                     {!! $post->post_content !!}
                 @endif
-                <p class="mb-0"><a href="/solliciteren/?vacature={!! urlencode($post->post_title) !!}" class="btn btn-md btn-primary mt-3" title="Ik meld me graag aan als vrijwilliger">Ik meld me graag aan als vrijwilliger<i class="fal fa-angle-right ml-2"></i></a></p>
+                <p class="mb-0"><a href="/solliciteren/" class="btn btn-md btn-primary mt-3" title="Ik meld me graag aan als vrijwilliger">Ik meld me graag aan als vrijwilliger<i class="fal fa-angle-right ml-2"></i></a></p>
             </div>
         </div>
     </div>
