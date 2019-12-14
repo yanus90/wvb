64784fc50763f20e7553812fea313e89163a31df
Yanick Nagtegaal <ynagtegaal@avancecommunicatie.nl>
2019-12-14 16:04:39 +0100

    Woocommerce template

--- web/app/themes/wvb-theme/resources/views/woocommerce/content-single-product.blade.php
+++ web/app/themes/wvb-theme/resources/views/woocommerce/content-single-product.blade.php
@@ -41,55 +41,59 @@ if ( post_password_required() ) {
     </div>
 </div>
 <div id="product-<?php the_ID(); ?>" <?php wc_product_class(); ?>>
-    <div class="row">
-        <div class="col-lg-8 col-xl-7 col-xxl-8">
-            <?php
-            /**
-             * Hook: woocommerce_before_single_product_summary.
-             *
-             * @hooked woocommerce_show_product_sale_flash - 10
-             * @hooked woocommerce_show_product_images - 20
-             */
-            do_action( 'woocommerce_before_single_product_summary' );
+    <div class="section-single-post section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-xl-7 col-xxl-8">
+                    <?php
+                    /**
+                     * Hook: woocommerce_before_single_product_summary.
+                     *
+                     * @hooked woocommerce_show_product_sale_flash - 10
+                     * @hooked woocommerce_show_product_images - 20
+                     */
+                    do_action( 'woocommerce_before_single_product_summary' );
 
-            /**
-             * Hook: woocommerce_after_single_product_summary.
-             *
-             * @hooked woocommerce_output_product_data_tabs - 10
-             * @hooked woocommerce_upsell_display - 15
-             * @hooked woocommerce_output_related_products - 20
-             */
-            do_action( 'woocommerce_after_single_product_summary' );
-            ?>
-        </div>
-        <div class="col-lg-4 col-xl-5 col-xxl-4">
-            <?php
-            /**
-             * Hook: woocommerce_single_product_summary.
-             *
-             * @hooked woocommerce_template_single_title - 5
-             * @hooked woocommerce_template_single_rating - 10
-             * @hooked woocommerce_template_single_price - 10
-             * @hooked woocommerce_template_single_excerpt - 20
-             * @hooked woocommerce_template_single_add_to_cart - 30
-             * @hooked woocommerce_template_single_meta - 40
-             * @hooked woocommerce_template_single_sharing - 50
-             * @hooked WC_Structured_Data::generate_product_data() - 60
-             */
-            do_action( 'woocommerce_single_product_summary' );
-            ?>
-            @if($payment_methods)
-                <div class="row pt-2 pb-2 mb-4">
-                    <div class="col">
-                        <p class="font-size-small">Beschikbare betaalmethoden:</p>
-                        <ul class="list-inline">
-                            <li class="list-inline-item">
-                                <img src="{{ get_template_directory_uri() }}/assets/images/payment-methods/ideal.svg" alt="iDeal" class="payment-logo">
-                            </li>
-                        </ul>
-                    </div>
+                    /**
+                     * Hook: woocommerce_after_single_product_summary.
+                     *
+                     * @hooked woocommerce_output_product_data_tabs - 10
+                     * @hooked woocommerce_upsell_display - 15
+                     * @hooked woocommerce_output_related_products - 20
+                     */
+                    do_action( 'woocommerce_after_single_product_summary' );
+                    ?>
                 </div>
-            @endif
+                <div class="col-lg-4 col-xl-5 col-xxl-4">
+                    <?php
+                    /**
+                     * Hook: woocommerce_single_product_summary.
+                     *
+                     * @hooked woocommerce_template_single_title - 5
+                     * @hooked woocommerce_template_single_rating - 10
+                     * @hooked woocommerce_template_single_price - 10
+                     * @hooked woocommerce_template_single_excerpt - 20
+                     * @hooked woocommerce_template_single_add_to_cart - 30
+                     * @hooked woocommerce_template_single_meta - 40
+                     * @hooked woocommerce_template_single_sharing - 50
+                     * @hooked WC_Structured_Data::generate_product_data() - 60
+                     */
+                    do_action( 'woocommerce_single_product_summary' );
+                    ?>
+                    @if($payment_methods)
+                        <div class="row pt-2 pb-2 mb-4">
+                            <div class="col">
+                                <p class="font-size-small">Beschikbare betaalmethoden:</p>
+                                <ul class="list-inline">
+                                    <li class="list-inline-item">
+                                        <img src="{{ get_template_directory_uri() }}/assets/images/payment-methods/ideal.svg" alt="iDeal" class="payment-logo">
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    @endif
+                </div>
+            </div>
         </div>
     </div>
 {{--    @include('woocommerce.single-product.add-to-cart.cart-mobile')--}}
