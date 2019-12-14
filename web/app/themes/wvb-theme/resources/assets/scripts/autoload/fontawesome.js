2ef6a424b80dda744acc54d5d50f36f9ee100c46
Yanick Nagtegaal <ynagtegaal@avancecommunicatie.nl>
2019-12-14 15:04:38 +0100

    Core updates

---
+++ web/app/themes/wvb-theme/resources/assets/scripts/autoload/fontawesome.js
@@ -0,0 +1,11 @@
+// import then needed Font Awesome functionality
+// import { library, dom } from '@fortawesome/fontawesome-svg-core';
+// // import the Github and Twitter icons
+// import { faChevronDoubleRight, faBars, faTimes, faPhone, faUserCircle } from '@fortawesome/pro-regular-svg-icons';
+// import { faEnvelope, faFileAlt } from '@fortawesome/pro-light-svg-icons';
+//
+// // add the imported icons to the library
+// library.add(faChevronDoubleRight, faBars, faTimes, faPhone, faUserCircle, faEnvelope, faFileAlt);
+//
+// // tell FontAwesome to watch the DOM and add the SVGs when it detects icon markup
+// dom.watch();
