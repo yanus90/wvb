eff39d0f9845322a95f4956808baea485de9bf54
Yanick Nagtegaal <ynagtegaal@avancecommunicatie.nl>
2020-01-11 12:25:18 +0100

    Woocommerce

--- web/app/themes/wvb-theme/app/woocommerce-filters.php
+++ web/app/themes/wvb-theme/app/woocommerce-filters.php
@@ -22,6 +22,7 @@ add_filter( 'woocommerce_checkout_fields' , 'custom_override_checkout_fields' );
 
 // Our hooked in function - $fields is passed via the filter!
 function custom_override_checkout_fields( $fields ) {
+    unset($fields['billing']['billing_company']);
     unset($fields['billing']['billing_address_2']);
     unset($fields['billing']['billing_state']);
     unset($fields['shipping']['shipping_address_2']);
