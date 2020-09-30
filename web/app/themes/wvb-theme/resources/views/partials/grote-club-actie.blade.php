be8642743082314668c95a0e681e68691d1b068a
Yanick Nagtegaal <ynagtegaal@avancecommunicatie.nl>
2020-10-01 00:07:04 +0200

    Grote clubactie

--- web/app/themes/wvb-theme/resources/views/partials/grote-club-actie.blade.php
+++ web/app/themes/wvb-theme/resources/views/partials/grote-club-actie.blade.php
@@ -1,8 +1,8 @@
-<div class="container">
+<div class="container mt-4">
 	<div class="row">
 		<div class="col-12">
 			<a href="https://lot.clubactie.nl/lot/v.v.-warnsveldse-boys/320032" target="_blank" class="d-block" style="box-shadow: 0 0 15px 0 rgba(0, 0, 0, 0.15); border-radius: .4rem;">
-				<img src="@asset('images/grote-clubactie-loten-bestellen@2x.png')" alt="Logo Grote Clubactie" class="img-fluid" style="border-radius: .4rem;">
+				<img src="@asset('images/grote-clubactie-loten-bestellen@2x.png')" alt="Bestel hier online jouw Grote Clubactie loten" class="img-fluid" style="border-radius: .4rem;">
 			</a>
 		</div>
 	</div>
