f7081254e5f75bda91857accfb74475e6492b608
Yanick Nagtegaal <ynagtegaal@avancecommunicatie.nl>
2021-10-18 21:12:10 +0200

    Aanpassing boys connected link

--- web/app/themes/wvb-theme/resources/views/partials/bc-cta.blade.php
+++ web/app/themes/wvb-theme/resources/views/partials/bc-cta.blade.php
@@ -12,7 +12,7 @@
                         <h4 class="line-height-bigger color-black text-center font-size-p18 font-size-lg-p20 font-size-xl-p26">Ben jij al lid van Boys Connected?</h4>
                     </div>
                     <div class="col-md-3 col-lg-3 col-xl-3 col-xxl-3 mt-sm-2 mt-md-0 text-center text-md-right text-lg-right">
-                        <a href="https://www.boysconnected.nl/inschrijven" title="Meld je snel aan bij Boys Connected" target="_blank" class="btn btn-outline-dark btn-md vm">Meld je snel aan<i class="fal fa-angle-right ml-2"></i></a>
+                        <a href="https://boysconnected.warnsveldseboys.nl/index.php/52-2/" title="Meld je snel aan bij Boys Connected" target="_blank" class="btn btn-outline-dark btn-md vm">Meld je snel aan<i class="fal fa-angle-right ml-2"></i></a>
                     </div>
                 </div>
             </div>
