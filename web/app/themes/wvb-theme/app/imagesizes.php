9c6229461ba41ec25739070b80ee3ae460c604cb
Yanick Nagtegaal <ynagtegaal@avancecommunicatie.nl>
2022-09-09 16:21:29 +0200

    Aanpassing team foto afmeting

--- web/app/themes/wvb-theme/app/imagesizes.php
+++ web/app/themes/wvb-theme/app/imagesizes.php
@@ -3,6 +3,7 @@ add_image_size('slider_image_crop', 1005, 560, true);
 add_image_size('post_thumb', 550, 360, true);
 add_image_size('post_image', 900, 600, true);
 add_image_size('team_image', 1024, 685, true);
+add_image_size('team_image_large', 1600, 1070, true);
 add_image_size('team_thumb', 660, 442, true);
 add_image_size('event_thumb_small', 660, 440, true);
 add_image_size('about_wvb_home_image', 1000, 600, true);
