4893e5403b716653a5dd3153a69364ec1e04df9b
Yanick Nagtegaal <ynagtegaal@avancecommunicatie.nl>
2023-10-21 22:43:25 +0200

    Members

--- web/app/themes/wvb-theme/app/admin.php
+++ web/app/themes/wvb-theme/app/admin.php
@@ -40,3 +40,53 @@ function my_wp_login_logo()
     <?php
 }
 add_action('login_enqueue_scripts', __NAMESPACE__ . '\\my_wp_login_logo');
+
+// Remove dashboard menu-items based on user role
+add_action('admin_menu', function () {
+    if (current_user_can('website_beheerder')) {
+        remove_menu_page('edit.php?post_type=acf-field-group');
+        remove_menu_page('edit-comments.php');
+        remove_menu_page('tools.php');
+        remove_menu_page('plugins.php');
+        remove_menu_page('admin.php?page=roles');
+        remove_menu_page('wpcf7');
+        remove_menu_page('wpseo_dashboard');
+        remove_menu_page('password-protected');
+        remove_menu_page('members');
+        remove_menu_page('smush');
+
+        // Subpages
+        remove_submenu_page('index.php', 'update-core.php');
+        remove_submenu_page('admin.php', 'roles');
+        remove_submenu_page('themes.php', 'widgets.php');
+
+        $customizer_url = add_query_arg('return', urlencode(remove_query_arg(wp_removable_query_args(), wp_unslash($_SERVER['REQUEST_URI']))), 'customize.php');
+        remove_submenu_page('themes.php', $customizer_url);
+    }
+});
+
+// Hide WP Mail SMTP based on user role
+add_action('admin_menu', function () {
+    if (current_user_can('website_beheerder')) {
+        remove_menu_page('wp-mail-smtp');
+        remove_submenu_page('wp-mail-smtp', 'wp-mail-smtp');
+        remove_submenu_page('wp-mail-smtp', 'wp-mail-smtp-logs');
+        remove_submenu_page('wp-mail-smtp', 'wp-mail-smtp-reports');
+        remove_submenu_page('wp-mail-smtp', 'wp-mail-smtp-tools');
+        remove_submenu_page('wp-mail-smtp', 'wp-mail-smtp-about');
+        remove_submenu_page('wp-mail-smtp', 'https://wpmailsmtp.com/lite-upgrade/?utm_source=WordPress&utm_medium=admin-menu&utm_campaign=liteplugin&utm_content=Upgrade%20to%20Pro');
+    }
+}, 2147483647);
+
+// Disable widget based on user role
+add_action('wp_dashboard_setup', function () {
+    $screen = get_current_screen();
+    if ( !$screen ) {
+        return;
+    }
+
+    // Remove the "WP Mail SMTP" widget based on user role
+    if (current_user_can('website_beheerder')) {
+        remove_meta_box('wp_mail_smtp_reports_widget_lite', 'dashboard', 'normal');
+    }
+}, 20);
