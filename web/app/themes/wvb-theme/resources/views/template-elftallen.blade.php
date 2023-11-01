a78bdf19d82a6ccffe33c2b9861905d2f2afd33f
Yanick Nagtegaal <ynagtegaal@avancecommunicatie.nl>
2023-11-01 18:04:44 +0100

    Elftallen slug

--- web/app/themes/wvb-theme/resources/views/template-elftallen.blade.php
+++ web/app/themes/wvb-theme/resources/views/template-elftallen.blade.php
@@ -16,7 +16,7 @@
                 @foreach($categories as $category)
                     <div class="col-12 col-md-6 col-lg-4 col-xxl-3 mb-4">
                         @php $image = get_field('afbeelding', 'term_'.$category->term_id) @endphp
-                        <a href="/elftallen/{!! $category->slug !!}" class="card" title="{{ $category->name }}">
+                        <a href="/teams/{!! $category->slug !!}" class="card" title="{{ $category->name }}">
                             <img src="{!! wp_get_attachment_image_url($image['id'], 'team_thumb') !!}" class="card-img" alt="{{ $category->name }}">
                             <div class="card-img-overlay d-flex flex-column">
                                 <div class="mt-auto">
